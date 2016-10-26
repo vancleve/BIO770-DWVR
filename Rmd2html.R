c304be5b0f67e33f9496ae9c74e5c73a39dce4b9
Jeremy Van Cleve <jeremy.vancleve@gmail.com>
2016-10-26 09:48:57 -0400

    week 9

--- Rmd2html.R
+++ Rmd2html.R
@@ -6,6 +6,7 @@
 
 Rmd_files = c(Sys.glob("*.Rmd"), Sys.glob("*.rmd"))
 zip_files = Sys.glob("*.zip")
+datadir = "/Users/vancleve/science/teaching/current_class/"
 
 ## unzip all zip files
 for (zip in zip_files) {
@@ -27,6 +28,44 @@ html_with_errors$knitr$opts_chunk$error = TRUE
 html_with_errors$knitr$opts_chunk$R.options = list(max.print=200)
 
 for (rmd in Rmd_files) {
+    # print(rmd)
+  
+    lines = readr::read_lines(rmd)
+    textfile = paste(lines, collapse = '\n')
+    
+    # capture all read csv calls
+    csvmatch = stringr::str_match_all(textfile, "(read[_.]csv *\\(.*?\")(.*?\\.csv)(\".*?\\))")[[1]]
+    if (nrow(csvmatch))
+    {
+      for (i in 1:nrow(csvmatch))
+      {
+        # for each read csv, replace the file path
+        # print(csvmatch[i,3])
+        # print(paste(datadir, basename(csvmatch[i,3]), sep = ''))
+        # print(grepl(csvmatch[i,3], textfile))
+        textfile = sub(csvmatch[i,1], 
+                       paste(csvmatch[i,2], datadir, basename(csvmatch[i,3]), csvmatch[i,4], sep = ''), 
+                       textfile, fixed = TRUE)
+      }
+    }
+    
+    xlsmatch = stringr::str_match_all(textfile, "(read_excel *\\(.*?\")(.*?\\.xlsx?)(\".*?\\))")[[1]]
+    if (nrow(xlsmatch))
+    {
+      for (i in 1:nrow(xlsmatch))
+      {
+        # for each read csv, replace the file path
+        # print(xlsmatch[i,3])
+        # print(paste(datadir, basename(xlsmatch[i,3]), sep = ''))
+        # print(grepl(xlsmatch[i,3], textfile))
+        textfile = sub(xlsmatch[i,1], 
+                       paste(xlsmatch[i,2], datadir, basename(xlsmatch[i,3]), xlsmatch[i,4], sep = ''), 
+                       textfile, fixed = TRUE)
+      }
+    }
+    
+    write(textfile, rmd)
+    
     ## render html files to current folder using custom html document
     rmarkdown::render(rmd, output_format = html_with_errors, output_dir = '.')
 }
